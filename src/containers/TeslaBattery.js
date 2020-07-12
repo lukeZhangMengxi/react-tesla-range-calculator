62379563824c609739f56f9a0dd80f26e6d81c1c
Mengxi Zhang <zmx94824@gmail.com>
2020-07-11 22:54:39 -0700

    Passing param to from container to car component

--- src/containers/TeslaBattery.js
+++ src/containers/TeslaBattery.js
@@ -4,11 +4,27 @@ import TeslaNotice from '../components/TeslaNotice/TeslaNotice';
 import TeslaCar from '../components/TeslaCar/TeslaCar';
 
 class TeslaBattery extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      carstats: [],
+      config: {
+        speed: 55,
+        temperature: 20,
+        climate: true,
+        wheels: 19
+      }
+    }
+  }
+
   render() {
+    // ES6 Object destructuring Syntax,
+    // takes out required values and create references to them
+    const { config } = this.state;
     return (
       <form className="tesla-battery">
         <h1>Range Per Charge</h1>
-        <TeslaCar />
+        <TeslaCar wheelsize={config.wheels}/>
         <TeslaNotice />
       </form>
     )
